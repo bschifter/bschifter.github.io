42079e6f4b224eae289a02da70a425c88531e574
Josh Schifter <josh.schifter@bentley.com>
2024-03-21 20:18:18 -0400

    Update code and add images

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// @pjs preload must be used to preload the image
+
+/* @pjs preload="bank.png"; */
+
 var sketchProc = function (processingInstance) {
     with (processingInstance) {
         size(400, 400);
@@ -5,9 +9,11 @@ var sketchProc = function (processingInstance) {
         smooth();
 
         // END BOILER PLATE
+       
+        var bankImage = loadImage("bank.png");
+        var robberImage = loadImage("robber.png");
 
-
-        var Ball = function (config) {
+        var Ball = function(config){
             this.x = config.x;
             this.y = config.y;
             this.vx = 0;
@@ -18,9 +24,9 @@ var sketchProc = function (processingInstance) {
             this.ay = 0;
             this.gravity = 0.2;
         };
-
+        
         var keys = [];
-        var Level = function (platforms, startX, startY, endX, endY, endWidth, endHeight) {
+        var Level = function(platforms, startX, startY, endX, endY, endWidth, endHeight) {
             this.platforms = platforms;
             this.startX = startX;
             this.startY = startY;
@@ -28,9 +34,9 @@ var sketchProc = function (processingInstance) {
             this.endY = endY;
             this.endWidth = endWidth;
             this.endHeight = endHeight;
-
+            
         };
-        var Button = function (config) {
+        var Button = function(config) {
             this.buttonX = config.buttonX;
             this.buttonY = config.buttonY;
             this.buttonWidth = config.buttonWidth;
@@ -39,12 +45,15 @@ var sketchProc = function (processingInstance) {
             this.color2 = config.color2;
             this.color3 = config.color3;
         };
-        var playButton = new Button({ buttonX: 127, buttonY: 177, buttonWidth: 120, buttonHeight: 40, color1: 4, color2: 255, color3: 0 });
-
-        var controlButton = new Button({ buttonX: 127, buttonY: 237, buttonWidth: 120, buttonHeight: 40, color1: 4, color2: 255, color3: 0 });
-
+        var playButton = new Button({buttonX: 127, buttonY: 93, buttonWidth: 120, buttonHeight: 40, color1: 4, color2: 255, color3: 0});
+        
+        var controlButton = new Button({buttonX: 127, buttonY: 147, buttonWidth: 120, buttonHeight: 40, color1: 4, color2: 255, color3: 0});
+        
+        var settingButton = new Button({buttonX: 127, buttonY: 199, buttonWidth: 120, buttonHeight: 40, color1: 4, color2: 255, color3: 0});
+        
+        var buttons = [playButton, controlButton, settingButton];
         //Platforms
-        var Platform = function (config) {
+        var Platform = function(config) {
             this.x = config.x;
             this.y = config.y;
             this.width = config.width;
@@ -54,7 +63,7 @@ var sketchProc = function (processingInstance) {
             this.canKill = config.canKill;
             this.canMove = config.canMove;
         };
-        Platform.prototype.draw = function () {
+        Platform.prototype.draw = function() {
             if (this.canKill === false) {
                 fill(4, 255, 0);
                 rect(this.x, this.y, this.width, this.height);
@@ -64,176 +73,188 @@ var sketchProc = function (processingInstance) {
                 rect(this.x, this.y, this.width, this.height);
             }
         };
-
-        var Platform1 = new Platform({ x: 100, y: 100, width: 100, height: 100 });
+        
+        var Platform1 = new Platform({x: 100, y: 100, width: 100, height: 100});
         var platforms0 = [];
-        var platforms1 = [Platform1];
-        var platforms2 = [];
+        var platforms1 = [Platform1];    
+        var platforms2 = [];   
         var platforms3 = [];
-
+        
         var homeScreen = new Level(platforms0, 1, 1);
         var level1 = new Level(platforms1, 10, 340, 323, 35, 37, 37);
         var level2 = new Level(platforms2, 10, 340, 33, 25, 10, 10);
         var level3 = new Level(platforms3, 10, 380, 10, 10, 10, 10);
         var levels = [homeScreen, level1, level2, level3];
         var currentLevel = 0;
-
-        var blueBall = new Ball({ x: levels[currentLevel].startX, y: levels[currentLevel].startY });
-
-        Level.applyChangeInLevels = function () {
+        
+        var blueBall = new Ball ({x: levels[currentLevel].startX, y: levels[currentLevel].startY});
+        
+        Level.applyChangeInLevels = function() {
             if (blueBall.x > levels[currentLevel].endX && blueBall.x < levels[currentLevel].endX + levels[currentLevel].endWidth && blueBall.y > levels[currentLevel].endY && blueBall.y < levels[currentLevel].endY + levels[currentLevel].endHeight) {
-                currentLevel++;
-                blueBall.x = levels[currentLevel].startX;
-                blueBall.y = levels[currentLevel].startY;
-
-                blueBall.vx = 0;
-                blueBall.vy = 0;
-            }
+                    currentLevel ++;
+                    blueBall.x = levels[currentLevel].startX;
+                    blueBall.y = levels[currentLevel].startY;
+                    
+                    blueBall.vx = 0;
+                    blueBall.vy = 0;
+                }
         };
-        Level.drawTextAndEnd = function () {
+        Level.drawTextAndEnd = function() {
             fill(128, 96, 74);
             rect(levels[currentLevel].endX, levels[currentLevel].endY, levels[currentLevel].endWidth, levels[currentLevel].endHeight);
             if (currentLevel === 0) {
                 fill(0, 0, 0);
                 textSize(32);
-                text("The Escape", 109, 100, 400, 100);
+                text("The Escape", 109, 29, 400, 100);
                 fill(0, 0, 0);
-                text("Play", 156, 184, 100, 100);
+                text("Play", 156, 100, 100, 100);
                 textSize(20);
-                text("Made by 1145-901", 115, 345, 339, 100);
+                text("Made by 1145-901", 115, 364, 339, 100);
             }
             if (currentLevel === 1) {
-
+            
             }
             if (currentLevel === 2) {
-
+                
             }
-
+        
         };
-        Button.prototype.draw = function () {
+        
+        Level.drawImages = function() {
             if (currentLevel === 0) {
+            fill(117, 88, 0);
+            rect(-1, 350, 401, 50);
+            fill(5, 255, 0);
+            rect (-1, 345, 401, 5);
+            image(bankImage, 40, 244, 100, 100);
+            image(robberImage, 200, 250, 90, 90);
+            }
+        };
+        Button.prototype.draw = function() {
+            if(currentLevel === 0) {
                 fill(this.color1, this.color2, this.color3);
                 rect(this.buttonX, this.buttonY, this.buttonWidth, this.buttonHeight);
-            }
-        };
-
-        Button.prototype.applyMouse = function () {
-            if (currentLevel === 0 && mouseX > this.buttonX && mouseX < this.buttonX + this.buttonWidth && mouseY > this.buttonY && mouseY < this.buttonY + this.buttonHeight) {
+                if (currentLevel === 0 && mouseX > this.buttonX && mouseX < this.buttonX + this.buttonWidth && mouseY > this.buttonY && mouseY < this.buttonY + this.buttonHeight) {
                 this.color1 = 37;
                 this.color2 = 130;
-                if (mousePressed) {
-                    currentLevel = 1;
-                    blueBall.x = levels[currentLevel].startX;
-                    blueBall.y = levels[currentLevel].startY;
+                }
+                else {
+                    this.color1 = 4;
+                    this.color2 = 255;
                 }
             }
-            else {
-                playButton.color1 = 4;
-                playButton.color2 = 255;
+            
+        };
+        
+        Button.prototype.applyMouse = function() {
+            if (currentLevel === 0 && mouseX > this.buttonX && mouseX < this.buttonX + this.buttonWidth && mouseY > this.buttonY && mouseY < this.buttonY + this.buttonHeight) {
+                currentLevel = 1;
+                blueBall.x = levels[currentLevel].startX;
+                blueBall.y = levels[currentLevel].startY;
             }
         };
-
-        Platform.prototype.applyMovement = function () {
+        
+        Platform.prototype.applyMovement = function() {
             if (this.canMove === true) {
                 if (this.moveLeft === true) {
-                    this.x--;
-                    if (blueBall.y > this.y - 5 && blueBall.y < this.y + 1 && blueBall.x > this.x && blueBall.x < this.x + this.width) {
-                        blueBall.x--;
+                    this.x --;
+                    if (blueBall.y > this.y - 5 && blueBall.y < this.y + 1 && blueBall.x > this.x &&             blueBall.x < this.x + this.width) {
+                        blueBall.x --;
                     }
                 }
                 else {
-                    this.x++;
-                    if (blueBall.y > this.y - 5 && blueBall.y < this.y + 1 && blueBall.x > this.x && blueBall.x < this.x + this.width) {
-                        blueBall.x++;
+                    this.x ++;
+                    if (blueBall.y > this.y - 5 && blueBall.y < this.y + 1 && blueBall.x > this.x &&             blueBall.x < this.x + this.width) {
+                        blueBall.x ++;
                     }
                 }
                 if (this.x + this.width > 400) {
                     this.moveLeft = true;
                 }
-                if (this.x < 0) {
-                    this.moveLeft = false;
-                }
+                    if (this.x < 0) {
+                        this.moveLeft = false;
+                    }
             }
-
+            
         };
-        Ball.prototype.applyIntersect = function (platform) {
+        Ball.prototype.applyIntersect = function(platform) {
             if (platform.canKill === false && this.y > platform.y - 5 && this.y < platform.y + 1 && this.x > platform.x && this.x < platform.x + platform.width) {
                 this.y = platform.y - 5;
                 this.vy = 0;
-            }
-
+            } 
+            
             if (platform.canKill === true && this.y > platform.y - 5 && this.y < platform.y + 1 && this.x > platform.x && this.x < platform.x + platform.width) {
                 this.x = levels[currentLevel].startX;
                 this.y = levels[currentLevel].startY;
                 this.vx = 0;
                 this.vy = 0;
-
-            }
+                
+            } 
             if (platform.canKill === false && this.y > platform.y && this.y < platform.y + platform.height && this.x > platform.x - 5 && this.x < platform.x + 1) {
-                this.x = platform.x - 5;
-                this.vx = 0;
-            }
-
+                    this.x = platform.x - 5;
+                    this.vx = 0;
+                }
+                
             if (platform.canKill === true && this.y > platform.y && this.y < platform.y + platform.height && this.x > platform.x - 5 && this.x < platform.x + 1) {
-                this.x = levels[currentLevel].startX;
-                this.y = levels[currentLevel].startY;
-                this.vx = 0;
-                this.vy = 0;
-            }
-
+                    this.x = levels[currentLevel].startX;
+                    this.y = levels[currentLevel].startY;
+                    this.vx = 0;
+                    this.vy = 0;
+                }
+                
             if (platform.canKill === false && this.y > platform.y && this.y < platform.y + platform.height && this.x < platform.x + platform.width + 5 && this.x > platform.x + platform.width - 1) {
-                this.x = platform.x + platform.width + 5;
-                this.vx = 0;
-            }
-
+                    this.x = platform.x + platform.width + 5;
+                    this.vx = 0;
+                }
+                
             if (platform.canKill === true && this.y > platform.y && this.y < platform.y + platform.height && this.x < platform.x + platform.width + 5 && this.x > platform.x + platform.width - 1) {
-                this.x = levels[currentLevel].startX;
-                this.y = levels[currentLevel].startY;
-                this.vx = 0;
-                this.vy = 0;
-            }
-
+                    this.x = levels[currentLevel].startX;
+                    this.y = levels[currentLevel].startY;
+                    this.vx = 0;
+                    this.vy = 0;
+                }
+                
             if (platform.canKill === false && this.x > platform.x && this.x < platform.x + platform.width && this.y < platform.y + platform.height + 5 && this.y > platform.y + platform.height - 1) {
                 this.y = platform.y + platform.height + 5;
                 this.vy = 0;
             }
-
+            
             if (platform.canKill === true && this.x > platform.x && this.x < platform.x + platform.width && this.y < platform.y + platform.height + 5 && this.y > platform.y + platform.height - 1) {
                 this.x = levels[currentLevel].startX;
                 this.y = levels[currentLevel].startY;
                 this.vx = 0;
                 this.vy = 0;
-
+                
             }
         };
         //Movement
-        Ball.prototype.applyUserInput = function (platforms) {
-
+        Ball.prototype.applyUserInput = function(platforms) {
+            
             if (keys.includes(RIGHT)) {
-
+                
                 this.ax = this.acceleration;
             }
             else if (keys.includes(LEFT)) {
-                this.ax = -this.acceleration;
-            }
-            else {
-                this.ax = 0;
-            }
-            var intersect = false;
-            for (var i = 0; i < platforms.length; i++) {
-                if (keys.includes(UP) && this.y === platforms[i].y - 5 && this.x > platforms[i].x && this.x < platforms[i].x + platforms[i].width) {
-                    intersect = true;
-
+                    this.ax = -this.acceleration;
                 }
-                if (intersect === true) {
-                    this.ay = -6;
+            else  {
+                this.ax = 0;
+                }
+                var intersect = false;
+            for (var i = 0; i < platforms.length; i++){
+                if (keys.includes(UP) && this.y === platforms[i].y - 5 && this.x > platforms[i].x &&           this.x < platforms[i].x + platforms[i].width) {
+                    intersect = true;
+                    
+                }
+                if(intersect === true){
+                    this.ay = -6;  
                 }
                 else {
                     this.ay = 0;
                 }
             }
         };
-        Ball.prototype.applyBorders = function () {
+        Ball.prototype.applyBorders = function() {
             if (this.x < 5) {
                 this.x = 5;
                 this.vx = 0;
@@ -244,7 +265,6 @@ var sketchProc = function (processingInstance) {
             }
             if (this.y > 400) {
                 fill(0, 0, 0);
-                text("You Died!", 100, 100, 100, 100);
                 this.x = levels[currentLevel].startX;
                 this.y = levels[currentLevel].startY;
                 this.vy = 0;
@@ -252,65 +272,63 @@ var sketchProc = function (processingInstance) {
             }
         };
         //Applying Velocity and Drag
-        Ball.prototype.applyGravity = function () {
+        Ball.prototype.applyGravity = function() {
             this.vy += this.gravity;
         };
-        Ball.prototype.applyVelocity = function () {
+        Ball.prototype.applyVelocity = function() {
             this.x += this.vx;
             this.y += this.vy;
         };
-        Ball.prototype.applyAcceleration = function () {
+        Ball.prototype.applyAcceleration = function() {
             this.vx += this.ax;
             this.vy += this.ay;
         };
-        Ball.prototype.applyDrag = function () {
+        Ball.prototype.applyDrag = function() {
             if (this.vx > 0) {
                 if (this.vx < this.drag) {
                     this.vx = 0;
                 } else {
-                    this.vx -= this.drag;
+                this.vx -= this.drag;
                 }
             }
             if (this.vx < 0) {
                 if (this.vx > this.drag) {
                     this.vx = 0;
                 } else {
-                    this.vx += this.drag;
+                this.vx += this.drag;
                 }
             }
             if (this.vy > 0) {
                 if (this.vy < this.drag) {
                     this.vy = 0;
                 } else {
-                    this.vy -= this.drag;
+                this.vy -= this.drag;
                 }
             }
             if (this.vy < 0) {
                 if (this.vy > this.drag) {
                     this.vy = 0;
                 } else {
-                    this.vy += this.drag;
+                this.vy += this.drag;
                 }
             }
         };
-        Ball.prototype.draw = function () {
+        Ball.prototype.draw = function() {
             fill(0, 98, 255);
             if (currentLevel > 0) {
-                image(getImage("avatars/leafers-sapling"), this.x, this.y, 50, 50);
+                image(robberImage, this.x, this.y, 50, 50);
             }
         };
-
-        draw = function () {
-
+        
+        draw = function() {
+            
             var level = levels[currentLevel];
             var platforms = level.platforms;
-
+            
             for (var i = 0; i < platforms.length; i++) {
                 blueBall.applyIntersect(platforms[i]);
             }
-
-            playButton.applyMouse();
-            controlButton.applyMouse();
+            
             blueBall.applyUserInput(platforms);
             blueBall.applyBorders();
             blueBall.applyGravity();
@@ -320,10 +338,12 @@ var sketchProc = function (processingInstance) {
             for (var i = 0; i < platforms.length; i++) {
                 platforms[i].applyMovement();
             }
-
-            background(189, 253, 255);
+            
+            background(110, 115, 255);
             playButton.draw();
             controlButton.draw();
+            settingButton.draw();
+            Level.drawImages();
             Level.drawTextAndEnd();
             blueBall.draw();
             Level.applyChangeInLevels();
@@ -331,22 +351,26 @@ var sketchProc = function (processingInstance) {
                 platforms[i].draw();
             }
         };
-
-        keyPressed = function () {
+        
+        mousePressed = function() {
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].applyMouse();
+            }
+        };
+        keyPressed = function() {
             if (keys.includes(keyCode)) {
-
+                
             }
             else {
-                keys.push(keyCode);
+            keys.push(keyCode);
             }
         };
-
-        keyReleased = function () {
+        
+        keyReleased = function() {
             keys.splice(keys.indexOf(keyCode), 1);
-
+            
         };
-
-
+        
 
         // START BOILER PLATE
 
